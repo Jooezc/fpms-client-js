0d473110fd9051d24fff45d209c75e4be3bfc1bf
liangzcn <liangzcn@126.com>
2023-09-12 19:02:12 +0800

    feat: fix cocos import error when use local crypto

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import WebSocket from 'ws';
-// import crypto from './crypto';
+import crypto from 'crypto-js';
 
 const ALIVE_DELAY = 30 * 1000; // 30秒
 const PARTNER_ID_IOS = '14033'; // for iOS
@@ -415,17 +415,16 @@ class FpmsConnector {
 
   private getAbc(dataObj: any): string {
     if (!dataObj) return '';
-    // try {
-    //   const txtStr = JSON.stringify(dataObj);
-    //   const key = crypto.MD5(this.mdSalt).toString(crypto.enc.Hex);
-    //   const retStr = crypto.enc.Base64.stringify(crypto.HmacSHA256(txtStr, key));
-    //   console.log(key, txtStr, retStr);
-    //   return retStr;
-    // } catch (err) {
-    //   console.error(err);
-    //   return '';
-    // }
-    return 'sadsadsajdskafhkashfsaLkdjakhfksajdlskahdskahd';
+    try {
+      const txtStr = JSON.stringify(dataObj);
+      const key = crypto.MD5(this.mdSalt).toString(crypto.enc.Hex);
+      const retStr = crypto.enc.Base64.stringify(crypto.HmacSHA256(txtStr, key));
+      console.log(key, txtStr, retStr);
+      return retStr;
+    } catch (err) {
+      console.error(err);
+      return '';
+    }
   }
 
   private sign(dataObj: any): any {
