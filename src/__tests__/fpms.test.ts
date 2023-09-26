5cfe5f016edc3a1077de548a36d22fe956a6c7ef
liangzcn <liangzcn@126.com>
2023-09-26 19:36:47 +0800

    1.0.4

--- src/__tests__/fpms.test.ts
+++ src/__tests__/fpms.test.ts
@@ -1,14 +1,14 @@
 import { getService } from '../fpms';
 test('fpms test', async () => {
   const config = {
-    PLATFORMID: "50",
-    PARTNERID: "14033",
-    PROVIDERID: "165",
-    WSCLIENTDOMAIN: "tongitskingdom.com",
-    GAMEID: "5CB92264-2205-4869-860C-52F5F0799F99",
+    PLATFORMID: '50',
+    PARTNERID: '14033',
+    PROVIDERID: '165',
+    WSCLIENTDOMAIN: 'tongitskingdom.com',
+    GAMEID: '5CB92264-2205-4869-860C-52F5F0799F99',
     DEVICETYPE: 3,
-    WSCLIENTTYPE: 4
-  }
+    WSCLIENTTYPE: 4,
+  };
   console.log(`fpms=`, getService(config));
   // await fpms.initConfig({
   //   PLATFORMID: "50",
